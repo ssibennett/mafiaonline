6ad49d04f6ec0e8a08b178b73446e979cd0810bb
mtj0712 <mtj0712@gmail.com>
2017-09-11 00:36:29 -0700

    Basic response from main.py
    main.py can now respond to request by sending a meaningless html code.

--- python/main.py
+++ python/main.py
@@ -1,5 +1,11 @@
+import bottle
+
 def main():
-	pass
+	@bottle.route("/")
+	def index():
+		return "<h1>Index</h1>"
+	
+	bottle.run(host="localhost", port=8000, debug=True)
 
 if __name__ == "__main__":
 	main()
