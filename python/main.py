770bac7b41d9058fe8d944e1f83f5c3a10ca51aa
mtj0712 <mtj0712@gmail.com>
2017-09-18 16:27:33 +0900

    Edited python/main.py
    "main.py" can now respond to file requests.
    Now, every extra files(image, css, javascript, etc.) should go to website/static_file/ folder.
    
    I'll wait for others to finish their websites so that I can make response functions for each of the html files.

--- python/main.py
+++ python/main.py
@@ -1,10 +1,16 @@
+# Bottle Python Web Framework
+
 import bottle
 
 def main():
 	@bottle.route("/")
 	def index():
-		return "<h1>Index</h1>"
-	
+		return bottle.template("<h1>Index</h1>")
+
+	@bottle.route("/static_file/<filepath:path>")
+	def static_file_request(filepath):
+		return bottle.static_file(filepath, root="/website/static_file/")
+
 	bottle.run(host="localhost", port=8000, debug=True)
 
 if __name__ == "__main__":
